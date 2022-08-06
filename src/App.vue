31a934ec050c52b56b03d9c80191eed5c0f8e3a2
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-06 11:10:01 +0800

    TopNav样式更新

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-04 13:07:20
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-06 11:02:12
+ * @LastEditTime: 2022-08-06 11:08:33
  * @FilePath: \com-ui-1\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE 
 -->
@@ -10,8 +10,8 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <div>
-    <router-link to="/gorge2">gorge2 |</router-link>
-    <router-link to="/">首页 |</router-link>
+    <router-link to="/Doc">Doc |</router-link>
+    <router-link to="/">Home |</router-link>
   </div>
   <router-view></router-view>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
+/*
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2022-08-04 13:07:20
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2022-08-06 11:08:52
+ * @FilePath: \com-ui-1\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from 'vue-router'
-import gorge from './components/gorge.vue'
-import gorge2 from './components/gorge2.vue'
+import Home from './views/Home.vue'
+import Doc from './Views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history:history,
     routes:[
-        {path: '/', component:gorge},  //当用户访问根路径，就访问gorge
-        {path: '/gorge2', component:gorge2}
+        {path: '/', component:Home},  //当用户访问根路径，就访问gorge
+        {path: '/Doc', component:Doc}
     ]
 })
 
