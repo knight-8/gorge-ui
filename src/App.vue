d50cb56f1f55e7c53f53d3262b304ab840195652
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-08 21:20:10 +0800

    Home页面样式

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-04 13:07:20
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-06 11:08:33
+ * @LastEditTime: 2022-08-08 02:38:34
  * @FilePath: \com-ui-1\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE 
 -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <div>
     <router-link to="/Doc">Doc |</router-link>
     <router-link to="/">Home |</router-link>
@@ -17,13 +15,11 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,20 @@
+<!--
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2022-08-05 03:33:17
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2022-08-08 15:28:09
+ * @FilePath: \com-ui-1\src\views\Home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
     <div>
         <TopNav/>
-        <div class="topnav">
-            <div class="logo">LOGO</div>
-            <div class="menu">
-                <ul>
-                    <li>菜单1</li>
-                    <li>菜单2</li>
-                </ul>
-            </div>
-        </div>
         <div class="banner">
-            <h1>HomeUI</h1>
+            <h1>GorgeUI</h1>
             <h2>一个炫目的UI框架</h2>
             <p class="actions">
-                <a href="">Github</a>
-                <a href="">开始</a>
+                <a href="https://github.com/knight-8/gorge-ui">Github</a>
+                <router-link to="/Doc">开始</router-link>
             </p>
         </div>
     </div>
@@ -29,20 +28,23 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.topnav{
-    background: pink;
+.banner{
+    padding: 100px 0;
     display: flex;
-    padding: 16px;
-    > .logo {
-        max-width: 6em;
-        margin-right: auto;
-    }
-    > .menu {
-        display: flex;
-        white-space: nowrap;
-        flex-wrap: nowrap;
-        > li {
-            margin: 0 1em;
+    justify-content: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
         }
     }
 }
