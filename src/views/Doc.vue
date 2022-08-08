6df887962ca5e3f015b0bd37fa54acaac712858f
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-08 21:44:28 +0800

    初始化aside的HTML

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,22 +2,31 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-05 03:33:11
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-06 10:54:15
+ * @LastEditTime: 2022-08-08 21:43:26
  * @FilePath: \com-ui-1\src\views\Doc.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <TopNav/>
-    <div>
-        <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
-        </div>
-        <div class="contnet">
-            <h1>DocI</h1>
-            <aside>哈哈哈</aside>
-            <aside>主内容</aside>
-        </div>
+    <div class="content">
+        <aside>
+            <h2>组件列表</h2>
+            <ol>
+                <li>
+                    <router-link to="/doc/switch">Switch组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/button">Button组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/dialog">Dialog组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/tabs">Tabs组件</router-link>
+                </li>
+            </ol>
+        </aside>
+        <main>主内容</main>
     </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-05 03:33:17
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-08 15:28:09
+ * @LastEditTime: 2022-08-08 21:41:09
  * @FilePath: \com-ui-1\src\views\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
