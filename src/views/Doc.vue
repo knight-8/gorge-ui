962a693a259a44ce43bdaab0a5e0dafe4ce23225
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-12 00:57:01 +0800

    官网css样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,15 +2,16 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-05 03:33:11
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-11 21:56:17
+ * @LastEditTime: 2022-08-12 00:52:38
  * @FilePath: \com-ui-1\src\views\Doc.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <TopNav/>
+  <div class="layout">
+    <TopNav class="nav"/>
     <div class="content">
         <aside v-if="asideVisible">
-            <h2>组列表</h2>
+            <h2>组件列表</h2>
             <ol>
                 <li>
                     <router-link to="/doc/switch">Switch组件</router-link>
@@ -30,6 +31,8 @@
             <router-view/>
         </main>
     </div>
+  </div>
+    
 </template>
 
 <script lang="ts">
@@ -48,6 +51,31 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.layout{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    > .nav {
+        flex-shrink: 0;
+    }
+    > .content {
+        flex-grow: 1;
+        @media (max-width:500px){
+            padding-left: 0; 
+        }
+    }
+}
+.content {
+    display:flex;
+    > aside {
+        flex-shrink: 0;
+    }
+    > main{
+        flex-grow: 1;
+        padding : 16px;
+        background: white;
+    }
+}
 aside {
     background: lightblue;
     width: 150px;
@@ -65,6 +93,9 @@ aside {
       > li{
           padding: 4px 0;
       }
+    }
+    .main{
+        overflow: auto;
     }
 }
 
