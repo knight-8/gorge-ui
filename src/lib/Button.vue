432983f2c77cad0d88031f8885ce86b0c007f0b6
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-19 17:27:28 +0800

    创建Button组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,21 @@
 <!--
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
- * @Date: 2022-08-11 22:52:10
+ * @Date: 2022-08-18 18:34:14
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-11 22:53:06
- * @FilePath: \com-ui-1\src\components\Button.vue
+ * @LastEditTime: 2022-08-19 17:11:05
+ * @FilePath: \com-ui-1\src\lib\Button.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div>这是Button页面</div>
-    <h1>示例1</h1>
-    <div>
-        <button>
-            你好
-        </button>
-    </div>
+    <button>
+        <slot />
+    </button>
 </template>
-
 <script lang="ts">
-import Button from "../lib/Button.vue"
+
 export default({
-    components: {Button}
+    setup() {
+        
+    },
 })
 </script>
