82e83e31069a80bacf67f62470198b3ae6c898bf
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-22 12:26:45 +0800

    改进Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-11 22:52:10
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-11 22:53:06
+ * @LastEditTime: 2022-08-22 12:25:48
  * @FilePath: \com-ui-1\src\components\Button.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,15 +10,22 @@
     <div>这是Button页面</div>
     <h1>示例1</h1>
     <div>
-        <button>
+        <Button @click="onClick">
             你好
-        </button>
+        </Button><br>
+        
     </div>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default({
-    components: {Button}
+    components: {Button: Button},
+    setup(){
+        const onClick = ()=>{
+            console.log('你好'); 
+        }
+        return {onClick}
+    }
 })
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-18 18:34:14
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-19 17:11:05
+ * @LastEditTime: 2022-08-19 20:09:46
  * @FilePath: \com-ui-1\src\lib\Button.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
