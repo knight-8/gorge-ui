06f5c5a508fa1ec0318169516e02c134976e89af
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-29 02:02:42 +0800

    button基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -9,12 +9,10 @@
 <template>
     <div>这是Button页面</div>
     <h1>示例1</h1>
-    <div>
         <Button>你好</Button>
-        <Button theme="button">你好</Button><br>
+        <Button theme="button">你好</Button>
         <Button theme="link">你好</Button>
-        <Button theme="text">你好 \</Button>
-    </div>
+        <Button theme="text">你好</Button>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-18 18:34:14
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-28 12:29:30
+ * @LastEditTime: 2022-08-28 19:11:27
  * @FilePath: \com-ui-1\src\lib\Button.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -27,8 +27,40 @@ export default{
 }
 </script>
 
-<style scoped>
-div{
-    border:1px solid red;
+<style lang="scss" scoped>
+$h: 32px;
+$border-color:#d9d9d9;
+$color:#333;
+$blue:#40a9ff;
+$radius:4px;
+.gorge-button{
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+    & + & {
+        margin-left: 8px;
+    }
+    &:hover,
+    &:focus {
+        color: $blue;
+        border-color: $blue;
+    }
+    &:focus {
+        outline: none;
+    }
+    &::-moz-focus-inner{
+        border: 0;
+    }
 }
 </style>
