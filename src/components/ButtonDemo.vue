00e81a10ecc0659c8d3de0b71a7761ad212752bb
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-29 17:48:12 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-11 22:52:10
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-29 14:53:25
+ * @LastEditTime: 2022-08-29 15:36:57
  * @FilePath: \com-ui-1\src\components\Button.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -28,6 +28,22 @@
             <Button size="big" theme="text">大大</Button>
             <Button theme="text">普通</Button>
             <Button size="small" theme="text">小小</Button>  
+        </div>
+        <h2>示例3</h2>
+        <div>
+            <Button level="main">主要按钮</Button>
+            <Button>普通按钮</Button>
+            <Button level="danger">危险按钮</Button>  
+        </div> 
+        <div>
+            <Button theme="link" level="main">主要链接按钮</Button>
+            <Button theme="link">普通链接按钮</Button>
+            <Button theme="link" level="danger">危险链接按钮</Button>  
+        </div>
+        <div>
+            <Button size="big" level="main">主要文字按钮</Button>
+            <Button theme="text">普通文字按钮</Button>
+            <Button size="small" level="danger">危险文字按钮</Button>  
         </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -3,102 +3,151 @@ import { computed } from 'vue'
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-18 18:34:14
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-29 14:49:45
+ * @LastEditTime: 2022-08-29 17:47:17
  * @FilePath: \com-ui-1\src\lib\Button.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-
-    <button class="gorge-button"
-            :class="classes">
-        <slot />
-    </button>
-
-
+  <button class="gorge-button" :class="classes">
+    <slot />
+  </button>
 </template>
 <script lang="ts">
-import { computed } from '@vue/runtime-core';
+import { computed } from "@vue/runtime-core";
 
-export default{
-    props: {
-        theme: {
-            type: String,
-            default: 'button'
-        },
-        size: {
-            type: String,
-            default: 'normal'
-        }
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "button",
     },
-     setup(props) {
-            const { theme, size } = props;
-            const classes = computed(() => {
-                return {
-                    [`gorge-theme-${theme}`]: theme,
-                    [`gorge-size-${size}`]: size
-                }
-            });
-            return{ classes }
-        }
-}
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`gorge-theme-${theme}`]: theme,
+        [`gorge-size-${size}`]: size,
+        [`gorge-level-${level}`]: level,
+      };
+    });
+    return { classes };
+  },
+};
 </script>
 
 <style lang="scss">
 $h: 32px;
-$border-color:#d9d9d9;
-$color:#333;
-$blue:#40a9ff;
-$radius:4px;
-.gorge-button{
-    box-sizing: border-box;
-    height: $h;
-    padding: 0 12px;
-    cursor: pointer;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    white-space: nowrap;
-    background: white;
-    color: $color;
-    border: 1px solid $border-color;
-    border-radius: $radius;
-    box-shadow: $radius;
-    box-shadow: 0 1px 0 fade-out(black, 0.95);
-    & + & {
-        margin-left: 8px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+$red: red;
+$grey: grey;
+.gorge-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    border-color: $blue;
+    background: darken(white, 5%);
+    color: lighten($blue, 10%);
+  }
+  &.gorge-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+  }
+  &.gorge-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.gorge-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.gorge-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.gorge-theme-button {
+    &.gorge-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &:hover,
-    &:focus {
-        border-color: $blue;
-        background: darken(white, 5%);
-        color: lighten($blue, 10%);
+    &.gorge-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
     }
-    &.gorge-theme-text {
-        border-color: transparent;
-        box-shadow: none;
-        color: inherit;
+  }
+  &.gorge-theme-link {
+    &.gorge-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
-    &.gorge-theme-link {
-        border-color: transparent;
-        box-shadow: none;
-        color: $blue;
+  }
+  &.gorge-theme-text {
+    &.gorge-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
     }
-    &:focus {
-        outline: none;
+    &.gorge-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
-    
-    &::-moz-focus-inner{
-        border: 0;
-    }
-        &.gorge-size-big{
-            font-size:24px;
-            height: 48px;
-            padding: 0 16px;
-        }
-        &.gorge-size-small{
-            font-size: 12px;
-            height: 20px;
-            padding: 0 4px;
-        }
+  }
 }
 </style>
