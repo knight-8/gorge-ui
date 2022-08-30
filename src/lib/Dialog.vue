d5d927acfee8642ac1a896366574f9f2a9d010c8
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-30 14:36:20 +0800

    dialog css1

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -2,30 +2,108 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-30 13:20:24
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-30 13:39:09
+ * @LastEditTime: 2022-08-30 14:36:11
  * @FilePath: \com-ui-1\src\lib\Dialog.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div class="dialog-overlay"></div>
-    <div class="dialog-wrapper">
-        <header>标题</header>
-        <main>
-            <p>第一行字</p>
-            <p>第二行字</p>
-        </main>
-        <footer>
-            <Button>OK</Button>
-            <Button>Cancel</Button>
-        </footer>
+  <div class="gorge-dialog-overlay"></div>
+  <div class="gorge-dialog-wrapper">
+    <div class="gorge-dialog">
+      <header>标题 <span class="gorge-dialog-close"></span></header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button level="main">OK</Button>
+        <Button>Cancel</Button>
+      </footer>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Button from './Button.vue'
+import Button from "./Button.vue";
 export default {
-    components: {
-        Button
-    }
-}
+  components: {
+    Button,
+  },
+};
 </script>
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+
+.gorge-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+
+  > main {
+    padding: 12px 16px;
+  }
+
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
