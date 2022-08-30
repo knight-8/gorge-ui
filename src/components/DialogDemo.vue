500e2330fc286a7aaac754a47f0fdd7ba8459bd6
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-30 13:40:41 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,22 @@
+<!--
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2022-08-11 22:52:15
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2022-08-30 13:20:54
+ * @FilePath: \com-ui-1\src\components\DialogDemo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
     <div>这是Dialog页面</div>
+    <h1>示例1</h1>
+    <Dialog><Dialog/>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+    components: {
+        Dialog
+    }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2022-08-30 13:20:24
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2022-08-30 13:39:09
+ * @FilePath: \com-ui-1\src\lib\Dialog.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行字</p>
+            <p>第二行字</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+    components: {
+        Button
+    }
+}
+</script>
