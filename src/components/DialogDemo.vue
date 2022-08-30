23ee38b8ed863de573591b8dbbaf908302ed5dbc
jiajunwa@outlook.com <jiajunwa@outlook.com>
2022-08-30 18:56:11 +0800

    dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,15 +2,17 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-11 22:52:15
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-30 14:52:00
+ * @LastEditTime: 2022-08-30 18:55:09
  * @FilePath: \com-ui-1\src\components\DialogDemo.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div>这是Dialog页面</div>
     <h1>示例1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog :visible="x"><Dialog/>
+    <Button @click="toggle">对话框</Button>
+    <Dialog v-model:visible="x"
+        :ok="f1" :cancel="f2" 
+    ></Dialog>
 </template>
 
 <script lang="ts">
@@ -25,10 +27,14 @@ export default {
         const x = ref(false)
         const toggle = ()=> {
             x.value = !x.value
-            console.log('hi');
-            
         }
-        return {x, toggle}
+        const f1 = () => {
+
+        }
+        const f2 = () => {
+            return false  
+        }
+        return {x, toggle, f1, f2}
     }
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -2,26 +2,30 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-30 13:20:24
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-30 14:36:11
+ * @LastEditTime: 2022-08-30 18:54:31
  * @FilePath: \com-ui-1\src\lib\Dialog.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-<template v-if="visible">
-<div class="gorge-dialog-overlay"></div>
-  <div class="gorge-dialog-wrapper">
-    <div class="gorge-dialog">
-      <header>标题 <span class="gorge-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="gorge-dialog-overlay" @click="OnClickOverlay"></div>
+    <div class="gorge-dialog-wrapper">
+      <div class="gorge-dialog">
+        <header>
+            标题
+            <span @click="close" class="gorge-dialog-close"></span>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
   
 </template>
@@ -37,7 +41,38 @@ export default {
       type: Boolean,
       default: false,
     },
+    closeOnClickOverlay: {
+        type: Boolean,
+        default: true
+    },
+    ok: {
+        type: Function
+    },
+    cancel: {
+        type: Function
+    }
   },
+  setup(props, context) {
+      const close = () => {
+          context.emit('update:visible', false)
+      }
+      const OnClickOverlay = () => {
+          if(props.closeOnClickOverlay){
+            close()
+          }
+      }
+      const ok = () => {
+          if(props.ok && props.ok() !== false){
+            close() 
+          }
+      }
+      const cancel = () => {
+          if(props.cancel?.() !== false){
+            close()
+          }
+      }
+      return { close, OnClickOverlay, ok, cancel }
+  }
 };
 </script>
 
