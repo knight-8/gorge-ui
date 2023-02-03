7c664161cc558a559ec62a6fa2810b29b6e1fbe3
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-03 11:06:03 +0800

    创建Tabs的Tab组件

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2023-02-03 10:58:13
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2023-02-03 10:58:44
+ * @FilePath: \com-ui-1\src\lib\Tabs.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div>
+        Tabs 组件
+    </div>
+</template>
