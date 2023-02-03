ef77cd831e239c135e918b1b6555999b5601248c
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-03 20:02:47 +0800

    渲染Tabs内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,12 +2,35 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2023-02-03 10:58:13
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2023-02-03 10:58:44
+ * @LastEditTime: 2023-02-03 19:59:58
  * @FilePath: \com-ui-1\src\lib\Tabs.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div>
-        Tabs 组件
+        <div v-for='(t, index) in titles' :key='index'>{{t}}</div>
+        <component v-for="(c, index) in defaults" :is='c' :key="index"/>
+        
     </div>
 </template>
+
+<script lang='ts'> 
+import Tab from './Tab.vue'
+export default {
+    setup(props, context) {
+        const defaults = context.slots.default();
+        defaults.forEach((tag) => {
+            if(tag.type !== Tab){
+                throw new Error('Tabs 子标签必须是 Tab')
+            }
+        })
+        const titles = defaults.map((tag) => {
+            return tag.props.title
+        })
+        return { 
+            defaults,
+            titles
+        }
+    },
+}
+</script>
