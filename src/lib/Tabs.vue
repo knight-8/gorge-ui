35c4fde975ec9c78e1560577c24211d92e22303e
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-06 17:24:11 +0800

    使用CSS切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,17 +2,20 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2023-02-03 10:58:13
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2023-02-04 13:35:30
+ * @LastEditTime: 2023-02-06 17:11:09
  * @FilePath: \com-ui-1\src\lib\Tabs.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div class="gorge-tabs">
         <div class="gorge-tabs-nav">
-            <div class="gorge-tabs-nav-item" v-for='(t, index) in titles' :key='index'>{{t}}</div>
+            <div class="gorge-tabs-nav-item" :class="{selected: t===selected}"
+             v-for='(t, index) in titles' @click="select(t)" :key='index'>{{t}}</div>
         </div>
         <div class="gorge-tabs-content">
-            <component class="gorge-tabs-content-item" v-for="(c, index) in defaults" :is='c' :key="index"/>
+            <component class="gorge-tabs-content-item" 
+            :class="{ selected: c.props.title === selected }" 
+            v-for="c in defaults" :is='c' />
         </div>
     </div>
 </template>
@@ -20,6 +23,11 @@
 <script lang='ts'> 
 import Tab from './Tab.vue'
 export default {
+    props: {
+        selected: {
+            type: String
+        }
+    },
     setup(props, context) {
         const defaults = context.slots.default();
         defaults.forEach((tag) => {
@@ -30,9 +38,17 @@ export default {
         const titles = defaults.map((tag) => {
             return tag.props.title
         })
+        const current = defaults.filter((tag) => {
+            return tag.props.title === props.selected
+        })[0]
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+        }
         return { 
             defaults,
-            titles
+            titles,
+            select,
+            current
         }
     },
 }
@@ -58,12 +74,19 @@ $border-color: #d9d9d9;
                 margin-left: 0;
             }
             &.selected {
-                color: $blue; 
+                color: $blue;
             }
         }
     }
     &-content {
         padding: 8px 0;
+
+        &-item {
+            display: none;
+            &.selected {
+                display: block;
+            }
+        }
     }
 }
 </style>
