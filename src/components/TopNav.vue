33f686257d854e009d4b9b39fdc47cd9fefe5236
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-08 22:48:31 +0800

    添加背景渐变

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-07 17:12:51
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-10 15:18:51
+ * @LastEditTime: 2023-02-08 22:47:09
  * @FilePath: \com-ui-1\src\components\TopNav.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -38,7 +38,7 @@ export default {
 
 <style lang="scss" scoped>
 .topnav {
-    background: pink;
+    //background: pink;
     display: flex;
     padding: 16px;
     position: relative;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-05 03:33:17
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-08 21:41:09
+ * @LastEditTime: 2023-02-08 22:46:48
  * @FilePath: \com-ui-1\src\views\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div>
+    <div class="topnavAndBanner">
         <TopNav/>
         <div class="banner">
             <h1>GorgeUI</h1>
@@ -28,12 +28,15 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.topnavAndBanner{
+    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
 .banner{
     padding: 100px 0;
     display: flex;
     justify-content: center;
     flex-direction: column;
-    background: lightgreen;
+    //background: lightgreen;
     > .actions{
         padding: 8px 0;
         a {
