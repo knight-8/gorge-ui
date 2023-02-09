306b40dda154938c425f0bb04ed75efec48c5c60
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-10 02:35:13 +0800

    完善介绍、安装和使用页面的内容，引入github样式

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-04 13:07:20
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-29 11:01:12
+ * @LastEditTime: 2023-02-10 02:26:06
  * @FilePath: \com-ui-1\src\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,7 @@ import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import 'github-markdown-css'
 
 
 const app = createApp(App)
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,10 @@
 <template>
-    <div>欢迎来到 Install 页面</div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install Gorge-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add Gorge-ui</code></pre>
+    <p>下一节：<a href="#/doc/get-started">开始使用</a></p>
+  </article>
 </template>
