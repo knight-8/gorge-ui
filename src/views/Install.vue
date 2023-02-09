89ece44d2fb0e9fbff4d15646272637c8477ee4d
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-10 03:26:50 +0800

    支持markdown文件

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,10 +1,20 @@
+<!--
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2023-02-10 01:57:53
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2023-02-10 03:23:13
+ * @FilePath: \com-ui-1\src\views\Install.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <article class="markdown-body">
-    <h1>安装</h1>
-    <p>打开终端运行下列命令：</p>
-    <pre><code>npm install Gorge-ui</code></pre>
-    <p>或</p>
-    <pre><code>yarn add Gorge-ui</code></pre>
-    <p>下一节：<a href="#/doc/get-started">开始使用</a></p>
-  </article>
+  <article class="markdown-body" v-html="install"></article>
 </template>
+
+<script lang="ts">
+import install from '../../markdown/install.md'
+export default{
+  data(){
+    return {install}
+  }
+}
+</script>
