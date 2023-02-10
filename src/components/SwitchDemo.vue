f2eabefa38193a4b0d15018c25ce83db55664eb4
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-11 04:30:06 +0800

    抽离用户需要拷贝的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -2,26 +2,79 @@
  * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
  * @Date: 2022-08-10 20:45:08
  * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
- * @LastEditTime: 2022-08-18 14:50:09
+ * @LastEditTime: 2023-02-11 04:23:16
+ * @FilePath: \com-ui-1\src\components\SwitchDemo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<!--
+ * @Author: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @Date: 2022-08-10 20:45:08
+ * @LastEditors: jiajunwa@outlook.com jiajunwa@outlook.com
+ * @LastEditTime: 2023-02-11 04:21:18
  * @FilePath: \com-ui-1\src\components\SwitchDemo.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-     <div>
-    <Switch v-model:value="bool" />
+<div>
+  <h1>Switch 组件示例 </h1>
+  <div class="demo">
+    <Switch1Demo/>
   </div>
+  <div class="demo">
+    <h2>支持 disabled </h2>
+    <Switch2Demo/>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue'
 import Switch  from '../lib/Switch.vue'
+import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1demo.vue'
+import Switch2Demo from './Switch2demo.vue'
 
-export default ({
-    components: { Switch },
-    
-    setup(){
-      const bool = ref(false)
-      return {bool}
+export default {
+  components: {
+    Switch1Demo,
+    Switch2Demo,
+    Switch,
+    Button
+  },
+  setup() {
+    const bool = ref(false)
+    return {
+      bool
     }
-})
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
