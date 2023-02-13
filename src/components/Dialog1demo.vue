ebb604a7ff209466e2a639402d3920be87434133
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-14 01:14:47 +0800

    优化Dialog Demo

---
+++ src/components/Dialog1demo.vue
@@ -0,0 +1,46 @@
+<demo>
+常规使用
+</demo>
+<template>
+    <Button @click="toggle">对话框</Button>
+    <Dialog v-model:visible="x"
+        :ok="f1" :cancel="f2" >
+        <template v-slot:content>
+            <div>你好</div>
+            <div>hi</div>
+        </template>
+        <template v-slot:title>
+            <strong>加粗的标题</strong>
+        </template>
+    </Dialog>
+</template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+
+import {ref} from 'vue'
+
+export default {
+    components: {
+        Dialog,
+        Button
+    },
+        setup() {
+        const x = ref(false)
+        const toggle = ()=> {
+            x.value = !x.value
+        }
+        const f1 = () => {
+            return false
+        }
+        const f2 = () => {}
+        return {
+            x, 
+            toggle, 
+            f1, 
+            f2, 
+        }
+    }
+}
+</script>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -7,56 +7,25 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div>这是Dialog页面</div>
-    <h1>示例1</h1>
-    <Button @click="toggle">对话框</Button>
-    <Dialog v-model:visible="x"
-        :ok="f1" :cancel="f2" >
-        <template v-slot:content>
-            <div>你好</div>
-            <div>hi</div>
-        </template>
-        <template v-slot:title>
-            <strong>加粗的标题</strong>
-        </template>
-    </Dialog>
-    <h1>示例2</h1>
-    <Button @click="showDialog">对话框</Button>
+    <h1>Dialog 示例</h1>
+    <Demo :component="Dialog1Demo" />
+    <Demo :component="Dialog2Demo" />
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import { ref } from 'vue'
-import { openDialog } from '../lib/openDialog'
+import Demo from './Demo.vue'
+import Dialog1Demo from './Dialog1demo.vue'
+import Dialog2Demo from './Dialog2demo.vue'
+
 export default {
     components: {
-        Dialog, Button
+        Demo
     },
     setup() {
-        const x = ref(false)
-        const toggle = ()=> {
-            x.value = !x.value
+        return {
+            Dialog1Demo,
+            Dialog2Demo
         }
-        const f1 = () => {
-
-        }
-        const f2 = () => {
-              
-        }
-        const showDialog = () => {
-            openDialog({
-                title: "标题",
-                content: "你好",
-                ok(){
-                    console.log('ok');
-                },
-                cancel(){
-                    console.log('cancel');
-                }
-            })
-        }
-        return {x, toggle, f1, f2, showDialog}
     }
 }
 </script>
