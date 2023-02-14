0934e5d66ef92eede324d8eb33e9bad987db0e93
jiajunwa@outlook.com <jiajunwa@outlook.com>
2023-02-14 15:19:40 +0800

    显示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="codeVisible = !codeVisible">查看代码</Button>
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
       <pre class="language-html" v-html="html" />
@@ -37,10 +38,20 @@ export default {
       );
     });
     const codeVisible = ref(false);
+    const showCode = () => {
+      codeVisible.value = true;
+      console.log(codeVisible);
+    }
+    const hideCode = () => {
+      codeVisible.value = false;
+      console.log(codeVisible);
+    }
     return {
       Prism,
       html,
       codeVisible,
+      showCode,
+      hideCode
     };
   },
 };
